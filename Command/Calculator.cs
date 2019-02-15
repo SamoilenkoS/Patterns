ca0570ce6ae5ce3f075f763e2eeecdd4ed944c3c
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-02-15 18:15:41 +0200

    Command pattern implementation

---
+++ Command/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Command
+{
+    public class Calculator
+    {
+        private int _currentValue = 0;
+
+        public void Operation(char @operator, int operand)
+        {
+            switch (@operator)
+            {
+                case '+':
+                    _currentValue += operand;
+                    break;
+                case '-':
+                    _currentValue -= operand;
+                    break;
+                case '*':
+                    _currentValue *= operand;
+                    break;
+                case '/':
+                    if(operand!=0)
+                    {
+                        _currentValue /= operand;
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException("operand", "Cannot divide by zero!");
+                    }
+
+                    break;
+            }
+            Console.WriteLine(
+              $"Current value = {_currentValue,3} (following {@operator} {operand})");
+        }
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем пользователя.
+            User user = new User();
+
+            // Пусть он что-нибудь сделает.
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+
+            // Отменяем 4 команды
+            user.Undo(4);
+
+            // Вернём 3 отменённые команды.
+            user.Redo(3);
+
+            // Ждем ввода пользователя и завершаемся.
+            Console.ReadLine();
+        }
+    }
+}
