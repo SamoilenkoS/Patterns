0110e7528938751c7556ab7123e4d695280bc147
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-03-12 15:55:12 +0200

    Typo fix

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -12,11 +12,11 @@ namespace Composite
             var joe = new Person {Name = "Joe"};
             var jake = new Person { Name = "Jake" };
             var emily = new Person { Name = "Emily" };
-            var sophia = new Person { Name = "Emily" };
+            var sophia = new Person { Name = "Sophia" };
             var brian = new Person {Name = "Brian"};
             var oldBob = new Person {Name = "Old Bob"};
             var newBob = new Person {Name = "New Bob"};
-            var bobs = new Group {Members = {oldBob, newBob}};
+            var bobs = new Group {Name = "Bobs", Members = {oldBob, newBob}};
             var developers = new Group {Name = "Developers", Members = {joe, jake, emily, bobs}};
 
             var parties = new Group {Members = {developers, sophia, brian}};
