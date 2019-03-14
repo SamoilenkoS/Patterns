bdd1e0f0130a5da5aa2b4b33ce57eb72711ab1ac
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-03-14 10:19:10 +0200

    Facade implementation

---
+++ Facade/Facade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Facade
+    {
+        private readonly SubsystemA sybsystemA = new SubsystemA();
+        private readonly SubsystemB sybsystemB = new SubsystemB();
+        private readonly SubsystemC sybsystemC = new SubsystemC();
+
+        public void Operation1()
+        {
+            Console.WriteLine("Operation 1\n" +
+                              sybsystemA.A1() +
+                              sybsystemA.A2() +
+                              sybsystemB.B1());
+        }
+        public void Operation2()
+        {
+            Console.WriteLine("Operation 2\n" +
+                              sybsystemB.B1() +
+                              sybsystemC.C1());
+        }
+    }
+}
---
+++ Facade/SubsystemA.cs
@@ -0,0 +1,14 @@
+﻿namespace Facade
+{
+    internal class SubsystemA
+    {
+        internal string A1()
+        {
+            return "Subsystem A, Method A1\n";
+        }
+        internal string A2()
+        {
+            return "Subsystem A, Method A2\n";
+        }
+    }
+}
