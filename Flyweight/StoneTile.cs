ca2d49cea0ed66767128d7ae945b02bb56846533
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-03-15 10:38:20 +0200

    Flyweight implementation

---
+++ Flyweight/StoneTile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public class StoneTile : ITile
+    {
+        public static int ObjectCounter = 0;
+
+        private Brush paintBrush;
+
+        public StoneTile()
+        {
+            paintBrush = Brushes.Blue;
+            ++ObjectCounter;
+        }
+
+        public void Draw(Graphics g, int x, int y, int width, int height)
+        {
+            g.FillRectangle(paintBrush, x, y, width, height);
+        }
+    }
+}
---
+++ Flyweight/TileFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public class TileFactory
+    {
+        private static Dictionary<string, ITile> tiles = new Dictionary<string, ITile>();
+
+        static TileFactory()
+        {
+
+        }
+
+        public static ITile GetTile(string tileType)
+        {
+            switch (tileType)
+            {
+                case "Stone":
+                    if (!tiles.ContainsKey("Stone"))
+                    {
+                        tiles["Stone"] = new StoneTile();
+                    }
+                    return tiles["Stone"];
+            }
+
+            return null;
+        }
+    }
+}
