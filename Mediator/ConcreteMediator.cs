e2679b6acce2ab8bfabc01c5c4708e814d8a7d6f
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-03-15 12:23:36 +0200

    Mediator implementation

---
+++ Mediator/ConcreteMediator.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// The 'ConcreteMediator' class
+/// </summary>
+public class ConcreteMediator : Mediator
+{
+    public ConcreteColleague1 Colleague1 { private get; set; }
+
+    public ConcreteColleague2 Colleague2 { private get; set; }
+
+    public override void Send(string message, Colleague colleague)
+    {
+        if (colleague == Colleague1)
+        {
+            Colleague2.Notify(message);
+        }
+        else
+        {
+            Colleague1.Notify(message);
+        }
+    }
+}
