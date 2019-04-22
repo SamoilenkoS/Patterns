3190b442f9b092d45a7242dceb1af0338fa2e1b5
Sviatoslav Samoilenko <ssamo@softserveinc.com>
2019-04-22 19:45:17 +0300

    Observer impl

---
+++ Observer/Broker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Observer
+{
+    class Broker : IObserver
+    {
+        public string Name { get; set; }
+        private IObservable stock;
+        public Broker(string name, IObservable observers)
+        {
+            Name = name;
+            stock = observers;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object obj)
+        {
+            StockInfo sInfo = (StockInfo)obj;
+
+            if (sInfo.USD > 30)
+                Console.WriteLine("Brocker {0} sold dollars;  Dollar price: {1}", Name, sInfo.USD);
+            else
+                Console.WriteLine("Brocker {0} bought dollars;  Dollar price: {1}", Name, sInfo.USD);
+        }
+        public void StopTrade()
+        {
+            stock.RemoveObserver(this);
+            stock = null;
+        }
+    }
+}
---
+++ Observer/Stock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    class Stock : IObservable
+    {
+        private StockInfo stockInfo; // информация о торгах
+
+        private List<IObserver> observers;
+        public Stock()
+        {
+            observers = new List<IObserver>();
+            stockInfo = new StockInfo();
+        }
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.Update(stockInfo);
+            }
+        }
+
+        public void Market()
+        {
+            Random random = new Random();
+            stockInfo.USD = random.Next(20, 40);
+            stockInfo.Euro = random.Next(30, 50);
+            NotifyObservers();
+        }
+    }
+}
